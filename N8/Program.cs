68e2e06466b80a85d5e90c3fc98162d68d900dbe
Nadzeya <lustra2013@gmail.com>
2022-11-13 22:08:24 +0300

    N8

---
+++ N8/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N)
+//, а на выходе показывает все чётные числа от 1 до N. 
+
+
+Console.Write("Введите число : ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 0)
+{
+    num = -num;
+
+    for (int res = 0; res <= num; res = res + 2)
+    {
+        Console.WriteLine(-res);
+    }
+}
+else
+{
+for (int res = 2; res <= num; res = res + 2)
+    {
+        Console.WriteLine(res);
+    }
+}
+
+
