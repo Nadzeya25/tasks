dce6e802f129292bb5899d3af9a0647f771cbeda
Nadzeya <lustra2013@gmail.com>
2022-11-13 22:12:06 +0300

    N24

---
+++ N24/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 24: Напишите программу, которая принимает на вход число (А)
+ и выдаёт сумму чисел от 1 до А*/
+
+Console.Write("Введите N : ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write($"{N} => ");
+int count = 0;
+for (int i = 1; i <= N; i++)
+{
+    count = count + i; 
+}
+Console.WriteLine(count);
+
+
+
+
