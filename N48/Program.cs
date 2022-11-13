cba96594e2aff358644b547f94370842a0663abc
Nadzeya <lustra2013@gmail.com>
2022-11-14 00:13:34 +0300

    N48

---
+++ N48/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Задача 48: Задайте двумерный массив размера m на n, 
+каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран. 
+m = 3, n = 4. */
+Console.WriteLine("Введите количество строк массива m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов масива n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int [,]array = new int [m,n];
+void FillArray(int[,]box)
+{
+    for (int i = 0; i < box.GetLength(0); i++)
+    {
+        for (int j = 0; j < box.GetLength(1); j++)
+        {
+            box[i, j] = i+j;
+        }
+    }
+}
+
+void PrintArray(int[,]box)
+{
+   for (int i = 0; i < box.GetLength(0); i++)
+   {
+    for (int j = 0; j < box.GetLength(1); j++)
+    {
+      Console.Write($"{box[i,j]} \t");
+    }
+    Console.WriteLine();
+   } 
+}
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
