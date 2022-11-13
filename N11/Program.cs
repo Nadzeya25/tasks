f2d77d64df68796975d6fe1fd06601e3f70b20c8
Nadzeya <lustra2013@gmail.com>
2022-11-13 22:08:52 +0300

    N11

---
+++ N11/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая выводит
+// случайное трёхзначное число и удаляет вторую цифру этого числа.
+int num = new Random().Next(100, 1000);
+Console.WriteLine(num);
+int res = num/100*10 +num%10;
+Console.WriteLine(res);
+
+
+
