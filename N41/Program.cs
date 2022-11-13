fefb903e7a87dccee9f2278ce136107422ade885
Nadzeya <lustra2013@gmail.com>
2022-11-14 00:11:46 +0300

    N41

---
+++ N41/Program.cs
@@ -0,0 +1,40 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел.
+ Посчитайте, сколько чисел больше 0 ввёл пользователь.*/
+Console.WriteLine("Какое количество чисел нужно проверить? ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] massiv = new int[m];
+void FillArray(int[] box) // метод заполняет массив
+{
+    int count = 0;
+    int size = box.Length;
+    for (int index = 0; index < size; index++)
+    {
+        Console.WriteLine($"Введите {index + 1} число: ");
+        box[index] = Convert.ToInt32(Console.ReadLine());
+        if (box[index] > 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"Количество положительных чисел в данной последовательности равна {count}");
+}
+void PrintArray(int[] box)   // метод печатает
+{
+    Console.Write("[ ");
+    int size = box.Length;
+    for (int index = 0; index < size; index++)
+    {
+        if (index == size - 1)
+        {
+            Console.Write($"{box[index]} ]");
+        }
+        else
+        {
+            Console.Write($"{box[index]}, ");
+        }
+    }
+}
+
+
+FillArray(massiv);   //заполняет массив 
+PrintArray(massiv);
