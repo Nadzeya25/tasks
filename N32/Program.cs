2642d2a56c4e423258871c1d1d6702d5dcc4721f
Nadzeya <lustra2013@gmail.com>
2022-11-13 23:56:55 +0300

    N32

---
+++ N32/Program.cs
@@ -0,0 +1,52 @@
+﻿/*Задача 32: Напишите программу замена элементов массива:
+ положительные элементы замените на соответствующие отрицательные, и наоборот.*/
+
+void FillArray(int[] box) // метод заполняет массив
+{
+    //  
+    int size = box.Length;
+    for (int index = 0; index < size; index++)
+    {
+        box[index] = new Random().Next(-8, 20);    //заполняем числами от -8 до 19
+    }
+}
+void PrintArray(int[] box)  
+{
+    int size = box.Length;
+    for (int index = 0; index < size; index++)
+    {
+        if (index == size - 1)
+        {
+            Console.Write($"{box[index]} ]");
+        }
+        else
+        {
+            Console.Write($"{box[index]}, ");
+        }
+    }
+}
+Console.Write("[ ");
+int[] array = new int[8]; // 8 количество элементов - размер массива
+FillArray(array);   //заполняет массив 
+PrintArray(array);
+Console.WriteLine();
+
+void CoverArray(int[] box)  
+{
+    int size = box.Length;
+    for (int index = 0; index < size; index++)
+    {
+        if (index == size - 1)
+        {
+            Console.Write($"{-box[index]} ]");
+        }
+        else
+        {
+            Console.Write($"{-box[index]}, ");
+        }
+    }
+}
+Console.Write("[ ");
+CoverArray(array);
+
+
