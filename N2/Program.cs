681e32d42f9a41b56c913f915966c67348f88378
Nadzeya <lustra2013@gmail.com>
2022-11-13 22:07:00 +0300

    N2

---
+++ N2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее. 
+Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+if (A > B)Console.WriteLine(" A больше B");
+if (A < B)Console.WriteLine(" A меньше B ");
+if (A == B)Console.WriteLine(" A равно B ");
+
