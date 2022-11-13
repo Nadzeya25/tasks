2d1803df761dcbd64d14ebe3eb4675d0e9d0c964
Nadzeya <lustra2013@gmail.com>
2022-11-13 22:07:45 +0300

    N5

---
+++ N5/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача N5. Напишите программу, которая на вход принимает одно число (N),
+// а на выходе показывает все целые числа в промежутке от -N до N. 
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 0)
+{
+    int res = -num;
+    while (res <= num && res >= -num)
+    {
+        Console.Write($"{res} ");
+        res++;
+    }
+}
+else
+{
+int res = num;
+    while (res >= num && res <= -num)
+    {
+        Console.Write($"{res} ");
+        res++;
+    }
+}
+
