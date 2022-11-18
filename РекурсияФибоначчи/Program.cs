28bdf8711782b111b5e76088e7c5c113796f5921
Nadzeya <lustra2013@gmail.com>
2022-11-18 12:11:39 +0300

    добав

---
+++ РекурсияФибоначчи/Program.cs
@@ -0,0 +1,16 @@
+﻿// Рекурсия .Число Фибоначчи. Из Лекции 4. 
+double Fibonacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+for (int i = 1; i < 10; i++)
+{
+  Console.WriteLine(Fibonacci(i));  
+}
+
+
+
+
+
+
