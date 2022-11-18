e59c9b92946e5e5f9e18620565722b95dd9afdb0
Nadzeya <lustra2013@gmail.com>
2022-11-18 12:13:56 +0300

    add

---
+++ N63/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 63: Задайте значение N. Напишите программу,
+// которая выведет все натуральные числа в промежутке от 1 до N.
+
+Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int m = 1;
+void NumberRec(int a, int b)         
+{
+    if (a <= b)
+    {
+        Console.WriteLine(a);
+        NumberRec(a + 1, b);
+    }
+}
+NumberRec(m, n);
+
+
+
+
+////////////////////////////////////////////////////////////////////
+
+
+
+
